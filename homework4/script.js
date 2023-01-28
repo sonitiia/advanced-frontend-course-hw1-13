5c195f9d3182a9251135ce08b65b750faa18857c
Sofi Tymechko <sofiatymechko@gmail.com>
2023-01-29 01:38:40 +0200

    add pure functions

--- homework4/script.js
+++ homework4/script.js
@@ -1,1 +1,51 @@
-console.log("EH");
+const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
+const themes = ["Диференційне рівняння", "Теорія автоматів", "Алгоритми і структури даних"];
+const marks = [4, 5, 5, 3, 4, 5];
+
+getPairs = (classmates) => {
+    const boysArray = [];
+    const girlsArray = [];
+    for (let i = 0; i < classmates.length; i++) {
+        const element = classmates[i];
+        if (element.endsWith("а")) {
+            girlsArray.push(element);
+        } else {
+            boysArray.push(element);
+            
+        }
+    }
+
+    const pairsArray = [];
+    for (let i = 0; i < classmates.length/2; i++) {
+        pairsArray.push([boysArray[i], girlsArray[i]]);
+    }
+    return pairsArray;
+}
+
+const pairs = getPairs(students);
+console.log(pairs);
+
+assignProjects = (pairs, namesOfProjects) => {
+    const projects = [];
+    for (let i = 0; i < pairs.length; i++) {
+        const pair = pairs[i];
+        const project = [pair[0] + " і " + pair[1], namesOfProjects[i]];
+        projects.push(project);
+    }
+    return projects;
+}
+
+const projects = assignProjects(pairs, themes);
+console.log(projects);
+
+setMarks = (classmates, marksInOrder) => {
+    const studentsMarks = [];
+    for (let i = 0; i < marksInOrder.length; i++) {
+        studentMark = [classmates[i], marksInOrder[i]];
+        studentsMarks.push(studentMark);
+    }
+    return studentsMarks;
+}
+
+const studentsMarks = setMarks(students, marks);
+console.log(studentsMarks);
