5fbac85316623160a94141e132124baa3e24dcba
Sofi Tymechko <sofiatymechko@gmail.com>
2023-02-02 20:35:52 +0200

    add getModa function

--- homework5/script.js
+++ homework5/script.js
@@ -9,3 +9,36 @@ getRandomArray = (length, min, max) => {
 
 const randomArray = getRandomArray(8, 1, 100);
 console.log(randomArray);
+
+
+getModa = (...numbers) => {
+    const moda = {};
+    let max = 0 || null;
+    let count = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+        const el = numbers[i];
+
+        if (el % 1 != 0) {
+            continue;
+        }
+
+        if (moda[el]) { // el = key, moda[el] = value
+            moda[el]++;
+        } else {
+            moda[el] = 1;
+        }
+
+        if (count < moda[el]) {
+            max = el;
+            count = moda[el];
+        }
+    }
+
+    return max;
+}
+
+// const moda = getModa(6.4, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2);
+// const moda = getModa(6.4, 6.4);
+const moda = getModa(6.4, 6.4, 3, 0);
+console.log(moda);
